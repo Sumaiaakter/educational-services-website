1da477f0a4443132a69256d83636328c412069aa
sumaiaakter <sumaiamannan2013@gmail.com>
2021-10-05 08:21:17 -0500

    service added

--- src/components/University/University.js
+++ src/components/University/University.js
@@ -16,6 +16,15 @@ const University = () => {
     const handleOnChange = (e) => {
         console.log(e);
     }
+
+    // for services
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch(`./services.JSON`)
+            .then(res => res.json())
+            .then(data => setServices(data.services))
+    }, [])
     return (
         <div className="university-container">
             <h1 className="uni mb-5">Our University</h1>
@@ -39,6 +48,32 @@ const University = () => {
                                     <Link to={`/details/${university.id}`}>
                                         <div className="btn btn-success">Details</div>
                                     </Link>
+
+                                </div>
+                            </div>
+
+                        )
+                    }
+                </div>
+            </div>
+            <hr />
+
+            {/* for services */}
+
+            <h1 className="uni m-5">Our Services</h1>
+            <div className="universities">
+                <div className="row">
+                    {
+                        services?.slice(0, 6).map(service =>
+                            <div className="col-md-4">
+                                <div className="cart">
+                                    <div className="image mb-5">
+                                        <img className="w-100" src={service.img} alt="" />
+                                    </div>
+                                    <h2 className="text-primary">Service: {service.name}</h2>
+                                    <p>Facilities: {service.facilities}</p>
+                                    <p>Bill: {service.bill}</p>
+
 
                                 </div>
                             </div>
